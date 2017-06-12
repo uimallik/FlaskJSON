1d12e51101ed5ddc53bbe71a0d36bd3f8fc844d3
bashokku <iiakumarii@gmail.com>
2017-06-12 18:28:28 -0400

    Create dev.sh

--- dev.sh
+++ dev.sh
@@ -1,4 +1,4 @@
 docker build --no-cache --tag flask:latest --file Dockerfile .
-docker tag flask bashokku/flask:latest
-docker push bashokku/flask:latest
+docker tag flask bashokku/flask
+docker push bashokku/flask
 #tesr

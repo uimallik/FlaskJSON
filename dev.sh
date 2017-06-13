afc90f5d6d9aa4ccce77ea8d68e8cc87a6023034
bashokku <iiakumarii@gmail.com>
2017-06-13 11:50:46 -0400

    Create dev.sh

--- dev.sh
+++ dev.sh
@@ -1,5 +1,5 @@
 docker build --no-cache --tag flaskapp:latest --file Dockerfile .
-docker login --username=bashokku --password=Eswar@123
+docker login
 docker tag flaskapp bashokku/flaskapp
 docker push bashokku/flaskapp
 #docker push bashokku/flask

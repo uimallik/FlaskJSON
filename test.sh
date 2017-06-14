7a84c529ff649405119c5a225640b2a65fc35396
bashokku <iiakumarii@gmail.com>
2017-06-14 16:28:06 -0400

    Create test.sh

--- test.sh
+++ test.sh
@@ -3,17 +3,20 @@ docker run -d -p 5000:5000 bashokku/flaskapp
 /var/go/apache-jmeter-3.2/bin/jmeter -n -t /var/go/flask.jmx -l flasklpipe.jtl -j /var/go/jmeterpipe.log
 grep "Err" /var/go/jmeterpipe.log > /var/go/errorlog
 a=$(sed -e 's/\(^.*(\)\(.*\)\(%.*$\)/\2/' /var/go/errorlog)
-#echo $a
+echo $a
 var=$(echo $a 0 | awk '{print $1 + $2}')
-#echo $var
+echo $var
 if [ $var == 0 ];
 then
-#echo "insideif"
+echo "insideif"
+echo "var val" $var
 passed=1
-else
+echo "passedval" $passed
+else 
 echo "inside else"
 passed=0
-exit(1)
+echo "passedval" $passed
+exit 1 
 #echo $passed
 fi
 

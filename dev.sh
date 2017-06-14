d74c6ef34e5f4dbf4bc587d97ff2c94f58fd1f79
bashokku <iiakumarii@gmail.com>
2017-06-14 09:45:30 -0400

    Create dev.sh

--- dev.sh
+++ dev.sh
@@ -1,5 +1,5 @@
-docker build --no-cache --tag flaskapp:latest --file Dockerfile .
-docker tag flaskapp bashokku/flaskapp
+docker build --no-cache --tag bashokku/flaskapp:latest --file Dockerfile .
+#docker tag flaskapp bashokku/flaskapp
 docker push bashokku/flaskapp
 #docker push bashokku/flask
 #tesr

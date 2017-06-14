38e559ee1d9b0c3a41fa8f350492db6144f1d6e8
bashokku <iiakumarii@gmail.com>
2017-06-14 15:47:34 -0400

    Create test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 docker pull bashokku/flaskapp
-#docker run -d -p 5000:5000 bashokku/flaskapp
-/var/go/apache-jmeter-3.2/bin/jmeter -n -t /var/go/flask.jmx -l var/go/flasklpipe.jtl -j /var/go/jmeterpipe.log
+docker run -d -p 5000:5000 bashokku/flaskapp
+/var/go/apache-jmeter-3.2/bin/jmeter -n -t /var/go/flask.jmx -l flasklpipe.jtl -j /var/go/jmeterpipe.log
+#/var/go/apache-jmeter-3.2/bin/jmeter -n -t /var/go/flask.jmx -l var/go/flasklpipe.jtl -j /var/go/jmeterpipe.log
 #docker-compose up --build -d
 #test
 #nothing her

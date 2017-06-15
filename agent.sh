006722dd7584fb6e95f4e594b785339d21f905af
bashokku <iiakumarii@gmail.com>
2017-06-15 17:58:04 -0400

    Update agent.sh

--- agent.sh
+++ agent.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build --no-cache --tag bashokku/flaskapp:latest --file Dockerfile .
-#docker tag flaskapp bashokku/flaskapp
+docker tag flaskapp bashokku/flaskapp
 docker push bashokku/flaskapp

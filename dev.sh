19504b880a3f50ccb6673061626731712fbde1ea
bashokku <iiakumarii@gmail.com>
2017-06-15 10:14:09 -0400

    Create dev.sh

--- dev.sh
+++ dev.sh
@@ -22,11 +22,6 @@ exit 1
 #echo $passed
 fi
 
-#
-
-
-
-
 #/var/go/apache-jmeter-3.2/bin/jmeter -n -t /var/go/flask.jmx -l var/go/flasklpipe.jtl -j /var/go/jmeterpipe.log
 #docker-compose up --build -d
 #test
